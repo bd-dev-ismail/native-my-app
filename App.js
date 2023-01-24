c392da24a17f0909a2969c414c723493498c6107
bd-dev-ismail <ismailratul7@gmail.com>
2023-01-24 19:27:53 +0600

    so interesting

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.color}>
+        Open up App.js to start working on your app testing!
+      </Text>
+      <Home/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  color: {
+    color: "#fff",
+    backgroundColor: "#606060",
+    padding: "10px",
   },
 });
